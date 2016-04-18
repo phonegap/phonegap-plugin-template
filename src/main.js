fd50d5e973e3fceb46e8aa75e7fda44b73ccb589
Jesse MacFadyen <purplecabbage@gmail.com>
2016-04-18 06:21:48 -0700

    Checks params
    * ignore .vscode
    
    * parameters are used, usage is output if not present

--- src/main.js
+++ src/main.js
@@ -1,5 +1,28 @@
 #!/usr/bin/env node
 
-var parseArgs = require('minimist')
+var templateLib = require('./plugin-template.js');
 
-console.log("... because I'm always on the run");
+function showHelp() {
+    console.error("Usage: phonegap-plugin-create DIRECTORY NAME");
+    console.error("DIRECTORY : relative or absolute path to create the plugin in");
+    console.error("    DIRECTORY will be created if it does not exist.");
+    console.error("NAME : a name for your plugin, ex. cordova-plugin-myplugin");
+}
+
+var args = process.argv.slice(2);
+if(args.length > 0) {
+    var installPath = args[0];
+    var pluginName = args[1];
+    var pluginId = args[2]; // id is TODO:
+    var result = templateLib.create(installPath,pluginName,pluginId);
+    // TODO: check result for error, output some info if we were not successful
+}
+else {
+    showHelp();
+    process.exit(1);
+}
+
+
+
+
+
--- src/plugin-template.js
+++ src/plugin-template.js
@@ -1,5 +1,41 @@
 #!/usr/bin/env node
 
-console.log("And my mama said. That love's all that matters!")
+var shell = require('shelljs');
+var path = require('path');
+var fs = require('fs');
 
-exports = {};
+function getTemplatePath() {
+    var currPath = path.dirname(require.resolve('../'));
+    var parPath = path.resolve(currPath + '/../template/');
+    return parPath;
+}
+
+
+module.exports = {
+    create:function(destPath,name,id) {
+        
+        var templatePath = getTemplatePath();
+        // copy tempate files over
+        shell.cp('-r',templatePath + "/.",destPath);
+        // go there, and get a list of all the files
+        shell.cd(destPath);
+        var files = shell.find('.');
+        
+        // TODO: clean up / validate these values
+        var safeName = name || "PLUGIN_NAME";
+        var safeId = id || "PLUGIN_ID";
+        
+        for (var n = 0; n < files.length; n++) {
+            var fileName = files[n];
+            if (fs.lstatSync(fileName).isFile()) {
+                var contents = fs.readFileSync(fileName, 'utf-8');
+                contents = contents.replace("PLUGIN_NAME",name);
+                contents = contents.replace("PLUGIN_ID",id);
+                fs.writeFileSync(fileName, contents);
+            }
+        }
+
+        // TODO: file names should be replacable as well.
+        
+    }
+};
