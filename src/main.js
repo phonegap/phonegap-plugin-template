168cc12444e698d2ba36aa5620e65bcfef255ab9
Jesse MacFadyen <purplecabbage@gmail.com>
2017-01-06 00:31:56 -0800

    Prompt for license and validate it

--- src/main.js
+++ src/main.js
@@ -1,28 +1,50 @@
 #!/usr/bin/env node
 
 var templateLib = require('./plugin-template.js');
+var prompt = require( 'inquirer' ).prompt;
+var validateLicense = require('validate-npm-package-license');
+
+main();
+
+function main() {
+    var args = process.argv.slice(2);
+    if(args.length > 0) {
+        var installPath = args[0];
+        var pluginName = args[1];
+        var pluginId = args[2];
+
+        var options = { 'message' : 'license',
+                        'name'    : 'license',
+                        'default' : 'MIT',
+                        'validate': isValidLicense
+                       };
+
+        prompt([options]).then(function(result) {
+            templateLib.create(installPath,pluginName,pluginId,result.license);
+        });
+    }
+    else {
+        showHelp();
+        process.exit(1);
+    }
+}
 
 function showHelp() {
-    console.error("Usage: phonegap-plugin-create DIRECTORY NAME");
+    console.error("Usage: phonegap-plugin-create DIRECTORY NAME ID");
     console.error("DIRECTORY : relative or absolute path to create the plugin in");
     console.error("    DIRECTORY will be created if it does not exist.");
-    console.error("NAME : a name for your plugin, ex. cordova-plugin-myplugin");
+    console.error("NAME : a name for your plugin, ex. MyPlugin");
+    console.error("ID   : the unique id of your plugin, ex. myorg-plugin-myplugin");
 }
 
-var args = process.argv.slice(2);
-if(args.length > 0) {
-    var installPath = args[0];
-    var pluginName = args[1];
-    var pluginId = args[2]; // id is TODO:
-    var result = templateLib.create(installPath,pluginName,pluginId);
-    // TODO: check result for error, output some info if we were not successful
-}
-else {
-    showHelp();
-    process.exit(1);
+function errorsFromResult(res) {
+    var errors = [].concat(res.errors || []).concat(res.warnings || []);
+    var err = new Error('Oops, ' + errors.join(' \n and '));
+    return err;
 }
 
-
-
-
-
+// this returns a result the way inquirer.prompt expects it, bool-true or an error
+function isValidLicense(data) {
+    var result = validateLicense(data);
+    return result.validForNewPackages || errorsFromResult(result);
+}
