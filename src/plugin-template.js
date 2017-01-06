6a11ae1e2c82360f32fec83f57292647983e24d6
Jesse MacFadyen <purplecabbage@gmail.com>
2017-01-05 22:47:52 -0800

    Replace id in output files

--- src/plugin-template.js
+++ src/plugin-template.js
@@ -13,29 +13,32 @@ function getTemplatePath() {
 
 module.exports = {
     create:function(destPath,name,id) {
-        
+
         var templatePath = getTemplatePath();
         // copy tempate files over
         shell.cp('-r',templatePath + "/.",destPath);
         // go there, and get a list of all the files
         shell.cd(destPath);
         var files = shell.find('.');
-        
+
         // TODO: clean up / validate these values
         var safeName = name || "PLUGIN_NAME";
         var safeId = id || "PLUGIN_ID";
-        
+
         for (var n = 0; n < files.length; n++) {
             var fileName = files[n];
             if (fs.lstatSync(fileName).isFile()) {
                 var contents = fs.readFileSync(fileName, 'utf-8');
                 contents = contents.replace("PLUGIN_NAME",name);
+                contents = contents.replace("PLUGIN_LOWER_NAME",name.toLowerCase());
                 contents = contents.replace("PLUGIN_ID",id);
+                contents = contents.replace("PLUGIN_LOWER_ID",id.toLowerCase());
+
                 fs.writeFileSync(fileName, contents);
             }
         }
 
         // TODO: file names should be replacable as well.
-        
+
     }
 };
