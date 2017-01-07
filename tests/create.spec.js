88b2487d5b1dce3425beaa59f7cfa2b7cb25c29f
Jesse MacFadyen <purplecabbage@gmail.com>
2017-01-07 00:47:06 -0800

    Add some tests

---
+++ tests/create.spec.js
@@ -0,0 +1,42 @@
+
+var templateLib = require('../src/plugin-template.js');
+var shell = require('shelljs');
+
+
+describe("create", function() {
+    it("contains a create function", function() {
+        expect(templateLib.create).toBeDefined();
+        expect(typeof templateLib.create).toBe('function');
+    });
+
+
+});
+
+describe("Create ::", function() {
+
+    afterEach(function() {
+        shell.rm('-rf','tests/MyTestPlugin');
+    });
+
+    it("can create a plugin", function() {
+
+        templateLib.create('tests/MyTestPlugin','MyTestPlugin','test-my-plugin','Apache-2');
+        var package = require('../tests/MyTestPlugin/package.json');
+
+        expect(package).toBeDefined();
+        expect(package.name).toBe('MyTestPlugin'.toLowerCase());
+        expect(package.cordova.id).toBe('test-my-plugin');
+        expect(package.license).toBe('Apache-2');
+
+        // TODO: check package.name, package.cordova.id, package.license, version, ...
+    });
+
+    it("should not change cwd", function() {
+        var cwd = process.cwd();
+        templateLib.create('tests/MyTestPlugin','MyTestPlugin','test-my-plugin','Apache-2');
+
+        expect(cwd).toBe(process.cwd());
+    });
+
+});
+
