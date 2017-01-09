3a32084a4f440927632de2f077d137da589852be
Jesse MacFadyen <purplecabbage@gmail.com>
2017-01-08 22:15:39 -0800

    test actual export, like the docs say

--- tests/create.spec.js
+++ tests/create.spec.js
@@ -1,15 +1,14 @@
 
-var templateLib = require('../src/plugin-template.js');
+var templateLib = require('..');
 var shell = require('shelljs');
 
 
-describe("create", function() {
+describe("exports create function", function() {
+    // make sure we export what we say we do
     it("contains a create function", function() {
         expect(templateLib.create).toBeDefined();
         expect(typeof templateLib.create).toBe('function');
     });
-
-
 });
 
 describe("Create ::", function() {
@@ -34,7 +33,6 @@ describe("Create ::", function() {
     it("should not change cwd", function() {
         var cwd = process.cwd();
         templateLib.create('tests/MyTestPlugin','MyTestPlugin','test-my-plugin','Apache-2');
-
         expect(cwd).toBe(process.cwd());
     });
 
